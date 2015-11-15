5a8907cfc18d1a2641efb720a4dac72ca8245f9f
Al Bueche IV <albert.bueche.iv@gmail.com>
2015-11-14 20:53:36 -0800

    made more generic display, real-time json updates

--- recurse.js
+++ recurse.js
@@ -3,83 +3,85 @@ var recursiveApp = angular.module('recursiveApp', []);
 recursiveApp.controller('recursiveCtrl', function($scope) {
 	$scope.test="test";
 	
-	$scope.data = [{  
+	$scope.data = {
 		"BatchID":35,
-		"Product":{
-			"Description":"Thingamabob",
-			"ProductID":12,
-			"Components":[  
-				{
-					"Requirement":"Sprocket",
-					"Quantity":1,
-					"FulfilledBy":[
-						{  
-							"BatchID":87,
-							"Product":{  
-								"Description":"Sprocket",
-								"ProductID":3,
-								"Components":{  
-								}
-							}						
+		"Description":"Thingamabob",
+		"ProductID":12,
+		"Components":[
+			{
+				"Requirement":"Sprocket",
+				"Quantity":1,
+				"FulfilledBy":[
+					{ 
+						"BatchID":87,
+						"Product":{  
+							"Description":"Sprocket",
+							"ProductID":3,
+							"Components":[]
+						}						
+					}
+				]
+			},
+			{
+				"Requirement":"Gizmo",
+				"Quantity":5,
+				"FulfilledBy":[ 
+					{  
+						"BatchID":32,
+						"Product":{  
+							"Description":"Gizmo",
+							"ProductID":54,
+							"Components":[]
 						}
-					]
-				},
-				{
-					"Requirement":"Gizmo",
-					"Quantity":5,
-					"FulfilledBy":[ 
-						{  
-							"BatchID":32,
-							"Product":{  
-								"Description":"Gizmo",
-								"ProductID":54,
-								"Components":{  
-								}
-							}
-						},
-						{  
-							"BatchID":33,
-							"Product":{  
-								"Description":"Gizmo",
-								"ProductID":54,
-								"Components":{  
-
-								}
-							}
+					},
+					{  
+						"BatchID":33,
+						"Product":{
+							"Description":"Gizmo",
+							"ProductID":54,
+							"Components":[]
 						}
-					]
-				}
-			]
-		}		
-	}];
+					}
+				]
+			}
+		]
+	};
 	
 	$scope.toggle = function(field) {
 		field.expanded = !field.expanded;
 	};
 });
 
-recursiveApp.directive('batch', function() {
+recursiveApp.directive('product', function() {
 	return { 
 		restrict : 'E',
 		scope : { 
-			batch: '=*data',
+			label: '=*',
+			data: '=*',
 		},
-		template : '<div ng-include="\'batch.html\'"></div>'
+		template : '<div ng-include="\'product.html\'"></div>',
+		link: function(scope, element, attrs) {
+			scope.isObject = function(value) {
+				return angular.isObject(value);
+			};
+			scope.isArray = function(value) {
+				return angular.isArray(value);
+			};
+		}
 	};
-}).directive('product', function() {
-	return {
-		restrict : 'E',
-		scope : { 
-			product: '=*data',
-		},
-		template : '<div ng-include="\'product.html\'"></div>'
-	};
-}).directive('component', function() {
-	return {
-		restrict : 'E',
-		scope : { 
-			component: '=*data',
-		},
-		template : '<div ng-include="\'component.html\'"></div>'
-	};
-})
+}).directive('jsonText', function() {
+    return {
+        restrict: 'A',
+        require: 'ngModel',
+        link: function(scope, element, attr, ngModel) {            
+			function into(input) {
+				return JSON.parse(input);
+			}
+			function out(data) {
+				return JSON.stringify(data);
+			}
+			ngModel.$parsers.push(into);
+			ngModel.$formatters.push(out);
+        }
+    };
+});
